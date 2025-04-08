0aae97202f209189af9bd43a5e9562a828c263ec
Ariel <ariteomiran@gmail.com>
2025-04-08 14:29:20 -0300

    commit inicial

---
+++ src/main/java/com/ArielMelo/Desafio_Magalu_agendamento_notificacao_api/DesafioMagaluAgendamentoNotificacaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.ArielMelo.Desafio_Magalu_agendamento_notificacao_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioMagaluAgendamentoNotificacaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioMagaluAgendamentoNotificacaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ArielMelo/Desafio_Magalu_agendamento_notificacao_api/DesafioMagaluAgendamentoNotificacaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ArielMelo.Desafio_Magalu_agendamento_notificacao_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluAgendamentoNotificacaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
